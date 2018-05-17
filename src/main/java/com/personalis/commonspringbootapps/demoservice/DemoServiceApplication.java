973ac9bfed5155f11ec5fd2b45836ca9361e3050
Anand Nair <anair@personalis.com>
2018-05-16 22:27:27 -0700

    changed

--- src/main/java/com/personalis/commonspringbootapps/demoservice/DemoServiceApplication.java
+++ src/main/java/com/personalis/commonspringbootapps/demoservice/DemoServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoServiceApplication {
-	//Comment
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoServiceApplication.class, args);
 	}
